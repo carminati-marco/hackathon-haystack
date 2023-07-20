caf2288685007f6024e6a811ec37de1b7ccc8bfb
robertomena <Roberto.m@taboola.com>
2023-07-20 11:17:51 +0100

    added embedding and other context

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+from handler import setup_haystack, query_haystack
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -11,6 +11,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # setup_haystack()
+    # query_haystack("what is the price of Brooklyn Mirror Organizer")
+    # query_haystack("where can I buy Brooklyn Mirror Organizer")
+    # query_haystack("what dinning chairs are for sale")
+    query_haystack("What chairs are affordable and crafted on hardwood")
+    #query_haystack("Are there any Nike shoes for sale")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- processor.py
+++ processor.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from typing import Union, List, Optional, Literal
 
 from haystack import Document
@@ -8,8 +9,8 @@ class SkimlinksPreProcessor(PreProcessor):
     def process(
             self,
             documents: Union[dict, Document, List[Union[dict, Document]]],
-            clean_whitespace: Optional[bool] = None,
-            clean_header_footer: Optional[bool] = None,
+            clean_whitespace: Optional[bool] = True,
+            clean_header_footer: Optional[bool] = True,
             clean_empty_lines: Optional[bool] = None,
             remove_substrings: Optional[List[str]] = None,
             split_by: Optional[Literal["word", "sentence", "passage"]] = None,
@@ -18,9 +19,19 @@ class SkimlinksPreProcessor(PreProcessor):
             split_respect_sentence_boundary: Optional[bool] = None,
             id_hash_keys: Optional[List[str]] = None,
     ) -> List[Document]:
-        # print(documents)
+
+        mapped_values = OrderedDict()
+        mapped_values['merchant_name'] = "This business "
+        mapped_values['title'] = "sells the product "
+        mapped_values['brand'] = "of brand "
+        mapped_values['regprice'] = "the price is "
+        mapped_values['cat_mapping'] = "the item has been categorized as "
+        mapped_values['url'] = "the product can be found in the url "
+        mapped_values['descr'] = "the url describes the product as "
+
         c = ""
-        for i, j in documents.meta.items():
-            c += (f"{i} {j}. ")
+        for key, values in mapped_values.items():
+            if key in documents.meta:
+                c += f"{mapped_values[key]} {documents.meta[key]}. "
         documents.content += ' ' + c
         return super().process(documents, id_hash_keys=id_hash_keys)
